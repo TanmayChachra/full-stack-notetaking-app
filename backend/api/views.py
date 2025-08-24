c8e714b71e3706f2ed0d0798fba299d9b7bd7ae1
Tanmay_Chachra <thenaive2004@gmail.com>
2025-08-24 13:27:00 +0530

    (7) Updated 'api/views.py' to allow it to add new users now
    Used generic views to create API to create a new user using serialized data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()       #all create user to access all users so no entry is created if existing entry already exists
+    serializer_class = User             
+    permission_classes = [AllowAny]     #allow anyone (even if not authenticated) to create a new user
