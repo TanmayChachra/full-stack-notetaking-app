43be29a1952b0e0f7412fcfbefdb76d2d6440b0a
Tanmay_Chachra <thenaive2004@gmail.com>
2025-08-24 15:13:04 +0530

    (14) Updated 'api/views.py' to define views for Notes
    Added create and delete functionalities if the user is authenticated in views.py for Notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
+
+# View to create and view notes
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        '''Takes no args; Gets all notes written by the user'''
+        user = self.request.user    # Gets the user if he is authenticated (Django Magic!)
+        return Note.objects.filter(author=user)
+
+
+    def perform_create(self, serializer):
+        '''No args; A way to override inbuilt create function based on our needs'''
+        if serializer.is_valid():
+            serializer.save(author=self.request.user) # since author was read-only , hence manually passed
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Delete notes that user owns"""
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 # View to create new user
 class CreateUserView(generics.CreateAPIView):
